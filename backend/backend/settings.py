f65f280808324ae82b24fc57a3d6d1813791cb75
mzaraf <mz.araf@gmail.com>
2025-04-25 14:59:34 +0100

    edits

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,14 +118,5 @@ SIMPLE_JWT = {
 }
 
 # CORS configuration
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 CORS_ALLOW_CREDENTIALS = True
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://nasrdavms.netlify.app"
-]
-
-# Add this too:
-CORS_ALLOWED_ORIGINS = [
-    "https://nasrdavms.netlify.app"
-]
